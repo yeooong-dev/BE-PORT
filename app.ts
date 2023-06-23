02a9329f79b3002e82d2a909413c2a209fc8d6e6
yeooong-dev <sk2dud@gmail.com>
2023-06-23 14:56:58 +0900

    app.ts 초기 세팅 완료

--- app.ts
+++ app.ts
@@ -1,11 +1,21 @@
 const express = require("express");
 const path = require("path");
+const session = require("express-session");
 const app = express();
 const PORT = 3000;
 
 //body-parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// session
+app.use(
+    session({
+        secret: "your-secret-key",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
