c425ed20b265865d4b34e2f482d0e3080b71c07f
yeooong-dev <sk2dud@gmail.com>
2024-03-03 16:59:40 +0900

    leave

--- models/leave.ts
+++ models/leave.ts
@@ -2,48 +2,48 @@ import { DataTypes, Model } from "sequelize";
 import sequelize from "../config/database";
 
 interface LeaveAttributes {
-  id?: number;
-  date: string;
-  userId: number;
-  status: "APPROVED" | "PENDING" | "DENIED";
+    id?: number;
+    date: string;
+    userId: number;
+    status: "APPROVED" | "PENDING" | "DENIED";
 }
 
 class Leave extends Model<LeaveAttributes> implements LeaveAttributes {
-  public id!: number;
-  public date!: string;
-  public userId!: number;
-  public status!: "APPROVED" | "PENDING" | "DENIED";
+    public id!: number;
+    public date!: string;
+    public userId!: number;
+    public status!: "APPROVED" | "PENDING" | "DENIED";
 
-  static associate() {
-    this.belongsTo(sequelize.model("User"), { foreignKey: "userId" });
-  }
+    static associate() {
+        this.belongsTo(sequelize.model("User"), { foreignKey: "userId" });
+    }
 }
 
 Leave.init(
-  {
-    id: {
-      type: DataTypes.INTEGER.UNSIGNED,
-      autoIncrement: true,
-      primaryKey: true,
+    {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        date: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.ENUM("APPROVED", "PENDING", "DENIED"),
+            allowNull: false,
+        },
     },
-    date: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    userId: {
-      type: DataTypes.INTEGER.UNSIGNED,
-      allowNull: false,
-    },
-    status: {
-      type: DataTypes.ENUM("APPROVED", "PENDING", "DENIED"),
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    tableName: "leaves",
-    timestamps: false,
-  }
+    {
+        sequelize,
+        tableName: "leaves",
+        timestamps: false,
+    }
 );
 
 Leave.associate();
